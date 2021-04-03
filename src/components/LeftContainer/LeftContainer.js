747f77be5db6272a0a1675ad3fd64b31558d371c
JoshHaokip <joshhaokip@gmail.com>
2021-04-03 23:19:25 +0100

    Building out init structure of the website

---
+++ src/components/LeftContainer/LeftContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const LeftContainer = () => {
+    return (
+        <div>
+
+            <div class="top-bar">
+
+                <div class="logo">
+                    <h1>EAST</h1>
+                    <h1>CAFE</h1>
+                </div>
+            </div>    
+            
+        </div>
+    )
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {LeftContainer} from '../../components'
+
+export const Home = () => {
+    return (
+        <div>
+
+            <LeftContainer />
+
+        </div>
+    )
+}
+
